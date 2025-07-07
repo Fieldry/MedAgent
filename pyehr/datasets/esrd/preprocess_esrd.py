3646bb2fd65665d9fb426ea8a145cd4610087bed
Fieldry <wangzx@stu.pku.edu.cn>
2025-07-08 01:11:44 +0800

    feat: update preprocess esrd dataset

--- pyehr/datasets/esrd/preprocess_esrd.py
+++ pyehr/datasets/esrd/preprocess_esrd.py
@@ -37,15 +37,27 @@ grouped = df.groupby('PatientID')
 patients = np.array(list(grouped.groups.keys()))
 
 # Get the patient IDs and outcomes
-patients = np.array(list(grouped.groups.keys()))
+should_in_test_set = [215, 265, 318, 740, 455, 370, 598, 616, 812, 998]
+patients = np.array([patient for patient in patients if patient not in should_in_test_set])
 patients_outcome = np.array([grouped.get_group(patient_id)['Outcome'].iloc[0] for patient_id in patients])
 
-# Randomly select 200 patients for the test set
-train_val_patients, test_patients = train_test_split(patients, test_size=200, random_state=SEED, stratify=patients_outcome)
+# Randomly select 300 patients for the test set
+train_val_patients, test_patients = train_test_split(patients, test_size=290, random_state=SEED, stratify=patients_outcome)
+test_patients = np.concatenate([test_patients, should_in_test_set])
 
 # Get the remaining patients for the train/val set
 train_val_patients_outcome = np.array([grouped.get_group(patient_id)['Outcome'].iloc[0] for patient_id in train_val_patients])
 train_patients, val_patients = train_test_split(train_val_patients, test_size=1/8, random_state=SEED, stratify=train_val_patients_outcome)
+
+# Print the sizes of the datasets
+print("Train patients size:", len(train_patients))
+print("Validation patients size:", len(val_patients))
+print("Test patients size:", len(test_patients))
+
+# Assert there is no data leakage
+assert len(set(train_patients) & set(val_patients)) == 0, "Data leakage between train and val sets"
+assert len(set(train_patients) & set(test_patients)) == 0, "Data leakage between train and test sets"
+assert len(set(val_patients) & set(test_patients)) == 0, "Data leakage between val and test sets"
 
 # Create train, val, test, dataframes
 train_df = df[df['PatientID'].isin(train_patients)]
@@ -104,6 +116,16 @@ test_data = [{
     'y_mortality': [y[0] for y in y_item],
 } for id_item, x_item, x_llm_item, record_time_item, missing_mask_item, y_item in zip(test_pid, test_x, test_raw_x, test_record_time, test_missing_mask, test_y)]
 
+# Split test data into 3 parts: 150 patients for train, 50 patients for val, 100 patients for test
+test_patients_outcome = np.array([grouped.get_group(patient_id)['Outcome'].iloc[0] for patient_id in test_patients])
+sub_train_val_patients, sub_test_patients = train_test_split(test_patients, test_size=100, random_state=SEED, stratify=test_patients_outcome)
+sub_train_val_patients_outcome = np.array([grouped.get_group(patient_id)['Outcome'].iloc[0] for patient_id in sub_train_val_patients])
+sub_train_patients, sub_val_patients = train_test_split(sub_train_val_patients, test_size=1/4, random_state=SEED, stratify=sub_train_val_patients_outcome)
+
+sub_train_data = [item for item in test_data if item['id'] in sub_train_patients]
+sub_val_data = [item for item in test_data if item['id'] in sub_val_patients]
+sub_test_data = [item for item in test_data if item['id'] in sub_test_patients]
+
 # Create the directory to save the processed data
 save_dir = os.path.join(processed_data_dir, 'split')
 os.makedirs(save_dir, exist_ok=True)
@@ -111,12 +133,18 @@ os.makedirs(save_dir, exist_ok=True)
 # Save the data to pickle files
 pd.to_pickle(train_data, os.path.join(save_dir, "train_data.pkl"))
 pd.to_pickle(val_data, os.path.join(save_dir, "val_data.pkl"))
-pd.to_pickle(test_data, os.path.join(save_dir, "test_data.pkl"))
+pd.to_pickle(test_data, os.path.join(save_dir, "fusion_data.pkl"))
+pd.to_pickle(sub_train_data, os.path.join(save_dir, "fusion_train_data.pkl"))
+pd.to_pickle(sub_val_data, os.path.join(save_dir, "fusion_val_data.pkl"))
+pd.to_pickle(sub_test_data, os.path.join(save_dir, "test_data.pkl"))
 
 # Print the sizes of the datasets
 print("Train data size:", len(train_data))
 print("Validation data size:", len(val_data))
-print("Test data size:", len(test_data))
+print("Fusion data size:", len(test_data))
+print("Fusion train data size:", len(sub_train_data))
+print("Fusion val data size:", len(sub_val_data))
+print("Test data size:", len(sub_test_data))
 
 # Export LOS statistics (calculated from the train set)
 pd.to_pickle(los_info, os.path.join(save_dir, "los_info.pkl"))
